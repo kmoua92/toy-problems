35c6e942de502301a27e8a6738164004a40cb50e
mjstromberg <michaeljstromberg@gmail.com>
2016-12-16 19:05:12 -0800

    (solution) Add solution to makingAnagrams

--- makingAnagrams/makingAnagrams.js
+++ makingAnagrams/makingAnagrams.js
@@ -17,5 +17,48 @@
  */
 
 var makingAnagrams = function makingAnagrams(a, b) {
-  
+  // initialize storage objects and deleteCount
+  var storageObjects = [ {}, {} ];
+  var deleteCount = 0;
+  // split strings into arrays
+  var stringArrays = [ a.split(''), b.split('') ];
+  // loop through each string
+
+  stringArrays.forEach(function(stringArray, storageArrayIndex) {
+    stringArray.forEach(function(char) {
+      // store character in respective storage object with count
+      if ( storageObjects[storageArrayIndex][char] ) {
+        storageObjects[storageArrayIndex][char] += 1;
+      } else {
+        storageObjects[storageArrayIndex][char] = 1;
+      }
+    });
+  });
+
+  // loop through each storage object
+  storageObjects.forEach(function(storageObject, storageObjectIndex) {
+    // get index of other storage object
+    var otherStorageObjectIndex = !storageObjectIndex ? 1 : 0;
+
+    for (var key in storageObject) {
+      // if character is not present in the other storage object
+      if ( storageObjects[ otherStorageObjectIndex ][key] === undefined ) {
+        // increment deleteCount by the count in the storage object
+        deleteCount += storageObject[key];
+        // delete the property from storage object
+        delete storageObject[key];
+      }
+      // if the counts in both storage objects are not equal
+      if ( storageObject[key] !== storageObjects[ otherStorageObjectIndex ][key] ) {
+        // increment deleteCount by the difference
+        deleteCount += Math.abs( storageObject[key] - storageObjects[otherStorageObjectIndex][key] );
+        // delete the property from both storage objects
+        delete storageObject[key];
+        delete storageObjects[otherStorageObjectIndex][key];
+      }
+    }
+  });
+
+  // return deleteCount
+  return deleteCount;
 };
