cbb47d72377e971a122e3618a808aacc168733ad
mjstromberg <michaeljstromberg@gmail.com>
2016-12-23 19:12:43 -0800

    (feature) Add dictionariesAndMaps toy problem

---
+++ dictionariesAndMaps/dictionariesAndMaps.js
@@ -0,0 +1,26 @@
+/*
+ *
+ * Given n names and phone numbers, assemble a phone book that maps friends' names to their
+ * respective phone numbers. You will then be given an unknown number of names to query your
+ * phone book for.
+ *
+ * Return an array that contains a result for each name in 'queries'. If there is an associated
+ * entry in your phone book, populate your result array with the phone book entry in the form of
+ * 'name=phoneNumber'. If an entry for name is not found, populate your result array with
+ * 'Not found' instead.
+ *
+ * Example:
+ *
+ * var phoneNumberString = 
+ *   'sam 99912222
+ *    tom 11122222
+ *    harry 12299933';
+ * 
+ * dictionariesAndMaps(3, phoneNumberString, ['sam', 'edward', 'harry'])
+ * // returns --> ['sam=99912222', 'Nof found', 'harry=12299933']
+ * 
+ */
+
+var dictionariesAndMaps = function dictionariesAndMaps(n, phoneNumbersString, queries) {
+
+};
