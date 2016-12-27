e86f5b7c1cf183f8bbe61cbbd2b3201f2b7462cb
mjstromberg <michaeljstromberg@gmail.com>
2016-12-26 22:20:44 -0800

    (feature) Add diagonalDifference toy problem

---
+++ diagonalDifference/diagonalDifference.js
@@ -0,0 +1,21 @@
+/*
+ * Given a square matrix of N x N size , calculate the absolute difference between the sums of
+ * its diagonals.
+ *
+ * Example:
+ * var matrix = [
+ *   [11, 2, 4],
+ *   [4, 5, 6],
+ *   [10, 8, -12]
+ * ];
+ *
+ * // primaryDiagonalSum = 11 + 5 + (-12);
+ * // secondaryDiagonalSum = 4 + 5 + 10;
+ *
+ * diagonalDifference(matrix); // returns 15
+ * 
+ */
+
+var diagonalDifference = function diagonalDifference(matrix) {
+  
+};
