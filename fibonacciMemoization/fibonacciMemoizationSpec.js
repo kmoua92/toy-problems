a18e97277ba4c887eb034c111d6e8ec7054d3a93
mjstromberg <michaeljstromberg@gmail.com>
2017-01-09 14:12:15 -0800

    (feature) Add tests for fibonacciMemoization

---
+++ fibonacciMemoization/fibonacciMemoizationSpec.js
@@ -0,0 +1,27 @@
+describe('fibonacciMemoization', function() {
+
+  it('should return a number', function() {
+    var result = fibonacciMemoization(5);
+
+    expect(result).to.be.a('number');
+  });
+
+  it('should return correct fibonacci number', function() {
+    var result = fibonacciMemoization(5);
+
+    expect(result).to.be.eql(5);
+  });
+
+  it('should handle n value of 39', function() {
+    var result = fibonacciMemoization(39);
+
+    expect(result).to.be.eql(63245986);
+  });
+
+  it('should have O(n) time complexity', function() {
+    var spy = sinon.spy(fibonacciMemoization);
+    var result = fibonacciMemoization(5);
+
+    expect(spy.callCount).to.be.eql(9);
+  });
+});
