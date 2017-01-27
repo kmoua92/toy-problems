8fe8bab3162769f739408482aa3b49f00a20907d
mjstromberg <michaeljstromberg@gmail.com>
2017-01-27 14:55:00 -0800

    (solution) Implement solution for selectionSort

--- selectionSort/selectionSort.js
+++ selectionSort/selectionSort.js
@@ -10,12 +10,21 @@
  */
 
 var selectionSort = function selectionSort(array) {
-  // store sortedSubarray, unsortedSubarray, and currentSmallest variables
+  // store sortedSubarray and unsortedSubarray variables
+  var sortedSubarray = [];
+  var unsortedSubarray = array.slice();
   // while length of sortedSubarray is less than length of array
+  while (sortedSubarray.length < array.length) {
     // loop through unsortedSubarray
-      // if currentValue is null or is less than currentSmallest
-        // set currentSmallest to currentValue
+    var currentSmallest = unsortedSubarray.reduce(function(smallest, currentValue, currentIndex) {
+      // if currentValue is null or is less than smallest return currentValue
+      return smallest === null || currentValue < smallest[0] ? [currentValue, currentIndex] : smallest; 
+    }, null);
     // append currentSmallest to sortedSubarray
-    // reset currentValue to null
-  // return sortedSubArray
+    sortedSubarray.push(currentSmallest[0]);
+    // remove currentSmallest from unsortedSubarray
+    unsortedSubarray.splice(currentSmallest[1], 1);
+  }
+  // return sortedSubarray
+  return sortedSubarray;
 };
