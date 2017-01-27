5ffbb8a0674fb9e13949726f53c1759f63973b35
mjstromberg <michaeljstromberg@gmail.com>
2017-01-27 13:37:01 -0800

    (feature) Add selectionSort toy problem

---
+++ selectionSort/selectionSort.js
@@ -0,0 +1,11 @@
+/*
+ *
+ * Create a sorting function that uses a selection sort strategy.
+ *
+ * For more information, visit: https://en.wikipedia.org/wiki/Selection_sort
+ * 
+ */
+
+var selectionSort = function selectionSort() {
+  
+};
