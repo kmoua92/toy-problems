51e2d80e6c03b2dff05d7c76cc979c2ee9bbecc0
mjstromberg <michaeljstromberg@gmail.com>
2017-01-27 13:44:01 -0800

    (feature) Update selectionSort description to reflect inputs and outputs

--- selectionSort/selectionSort.js
+++ selectionSort/selectionSort.js
@@ -1,11 +1,14 @@
 /*
  *
  * Create a sorting function that uses a selection sort strategy.
+ * 
+ * The input for the function should be an array (possibly alreadly sorted), and the output should
+ * be a sorted array.
  *
  * For more information, visit: https://en.wikipedia.org/wiki/Selection_sort
  * 
  */
 
-var selectionSort = function selectionSort() {
-  
+var selectionSort = function selectionSort(array) {
+
 };
