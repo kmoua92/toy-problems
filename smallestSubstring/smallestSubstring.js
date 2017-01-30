0aa5c85784a318add3091156f17aec55225d1e28
mjstromberg <michaeljstromberg@gmail.com>
2017-01-29 22:23:03 -0800

    (solution) Add preliminary pseudocode for smallestSubstring

--- smallestSubstring/smallestSubstring.js
+++ smallestSubstring/smallestSubstring.js
@@ -10,5 +10,23 @@
  */
 
 var smallestSubstring = function smallestSubstring(arr, str) {
-
+  // initialize a currentMap object with every character in arr as keys with values of false
+  // initialize shortestValidSubstring to ''
+  // initialize uniqueCharCount to 0
+  // initialize tailIndex to 0
+  // initialize headIndex to arr length - 1
+  // initialize currentIndex to 0
+  // while headIndex is less than str length and tailIndex is less than str length minus arr length
+    // loop through str from tailIndex to headIndex
+      // if currentChar in currentMap
+        // if currentMap[currentChar] is false
+          // increment uniqueCharCount
+        // set currentMap[currentChar] to true
+    // if uniqueCharCount equals arr length and substring between headIndex and tailIndex is less than shortestValidSubstring length
+      // set shortestValidSubstring to substring between headIndex and tailIndex
+    // else
+      // while uniqueCharCount is less than arr length
+        // increment headIndex
+        // if currentChar
+    // increment tailIndex
 };
