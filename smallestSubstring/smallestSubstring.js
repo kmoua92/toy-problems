d6753f760c6a78fcd0a7e4f55993e6a4a88c1ea0
mjstromberg <michaeljstromberg@gmail.com>
2017-01-30 13:52:41 -0800

    (fix) Add time and space complexity requirements to smallestSubstring

--- smallestSubstring/smallestSubstring.js
+++ smallestSubstring/smallestSubstring.js
@@ -2,6 +2,14 @@
  *
  * Given an array with unique characters arr and a string str, find the smallest substring of str
  * containing all characters of arr.
+ *
+ * Constraints:
+ *   If:
+ *     m = arr.length
+ *     n = str.length
+ *   Then:
+ *     Time Complexity must be O(m + n)
+ *     Space Complexity must be O(m)
  * 
  * Example:
  *   arr: [x,y,z], str: 'xyyzyzyx'
