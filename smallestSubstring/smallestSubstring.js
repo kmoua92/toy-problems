1de8cf53f5bb55925b46fab763948fe7d8091b2d
mjstromberg <michaeljstromberg@gmail.com>
2017-01-29 21:29:22 -0800

    (feature) Add smallestSubstring toy problem

---
+++ smallestSubstring/smallestSubstring.js
@@ -0,0 +1,14 @@
+/*
+ *
+ * Given an array with unique characters arr and a string str, find the smallest substring of str
+ * containing all characters of arr.
+ * 
+ * Example:
+ *   arr: [x,y,z], str: 'xyyzyzyx'
+ *   result: 'zyx'
+ * 
+ */
+
+var smallestSubstring = function smallestSubstring(arr, str) {
+
+};
