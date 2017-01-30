960115f0d15c4ae5fa376616acf46ed7dc224b92
mjstromberg <michaeljstromberg@gmail.com>
2017-01-30 11:22:39 -0800

    (solution) Update pseudocode for smallestSubstring

--- smallestSubstring/smallestSubstring.js
+++ smallestSubstring/smallestSubstring.js
@@ -10,23 +10,23 @@
  */
 
 var smallestSubstring = function smallestSubstring(arr, str) {
-  // initialize a currentMap object with every character in arr as keys with values of false
-  // initialize shortestValidSubstring to ''
+  // initialize a currentMap object with every character in arr as keys with values of 0
+  // initialize shortestValidSubstring to null
   // initialize uniqueCharCount to 0
   // initialize tailIndex to 0
-  // initialize headIndex to arr length - 1
-  // initialize currentIndex to 0
-  // while headIndex is less than str length and tailIndex is less than str length minus arr length
-    // loop through str from tailIndex to headIndex
-      // if currentChar in currentMap
-        // if currentMap[currentChar] is false
-          // increment uniqueCharCount
-        // set currentMap[currentChar] to true
-    // if uniqueCharCount equals arr length and substring between headIndex and tailIndex is less than shortestValidSubstring length
-      // set shortestValidSubstring to substring between headIndex and tailIndex
-    // else
-      // while uniqueCharCount is less than arr length
-        // increment headIndex
-        // if currentChar
-    // increment tailIndex
+  // loop from headIndex 0 to str length - 1
+    // if current character at headIndex is in currentMap
+      // if value associated with current character key in currentMap is 0
+        // increment uniqueCharCount by 1
+      // increment value associated with current character key in currentMap
+      // while uniqueCharCount is equal to arr length
+        // initialize currentLength variable to length between headIndex and tailIndex
+        // if currentLength is equal to arr length
+          // return substring between headIndex and tailIndex
+        // if shortestValidSubstring is null or currentLength is less than shortestValidSubstring length
+          // set shortestValidSubstring to substring between headIndex and tailIndex
+        // initialize tailChar to the character at tailIndex
+        // if tailChar is in currentMap and its associated value is 1
+          // decerement uniqueCharCount by 1
+        // increment tailIndex by 1
 };
