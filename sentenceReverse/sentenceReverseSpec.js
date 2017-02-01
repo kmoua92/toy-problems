2914faba50a9b9073dfdcc27128d25556fd78c17
mjstromberg <michaeljstromberg@gmail.com>
2017-02-01 15:12:57 -0800

    (fix) Fix sentenceReverseSpec to pass exampleInput by value

--- sentenceReverse/sentenceReverseSpec.js
+++ sentenceReverse/sentenceReverseSpec.js
@@ -1,41 +1,47 @@
 describe('sentenceReverse', function() {
 
-  var exampleInput = [ 'p', 'e', 'r', 'f', 'e', 'c', 't', '  ', 'm', 'a', 'k', 'e', 's', '  ', 'p', 'r', 'a', 'c', 't', 'i', 'c', 'e' ];
-  var exampleOutput = [ 'p', 'r', 'a', 'c', 't', 'i', 'c', 'e', '  ', 'm', 'a', 'k', 'e', 's', '  ', 'p', 'e', 'r', 'f', 'e', 'c', 't' ];
+  var exampleInput = [ 'p', 'e', 'r', 'f', 'e', 'c', 't', ' ', 'm', 'a', 'k', 'e', 's', ' ', 'p', 'r', 'a', 'c', 't', 'i', 'c', 'e' ];
+  var exampleOutput = [ 'p', 'r', 'a', 'c', 't', 'i', 'c', 'e', ' ', 'm', 'a', 'k', 'e', 's', ' ', 'p', 'e', 'r', 'f', 'e', 'c', 't' ];
 
   it('should return an array', function() {
-    var result = sentenceReverse(exampleInput);
+    var result = sentenceReverse(exampleInput.slice());
 
     expect(result).to.be.a('array');
   });
 
   it('should return a the correct reversed sentence for example', function() {
-    var result = sentenceReverse(exampleInput);
+    var result = sentenceReverse(exampleInput.slice());
 
-    expect(result).to.be.equal(exampleOutput);
+    expect(result).to.be.eql(exampleOutput);
   });
 
   it('should handle empty array', function() {
     var result = sentenceReverse([]);
 
-    expect(result).to.be.equal([]);
+    expect(result).to.be.eql([]);
   });
 
   it('should handle array with no spaces', function() {
     var result = sentenceReverse(['h', 'i']);
 
-    expect(result).to.be.equal(['h', 'i']);
+    expect(result).to.be.eql(['h', 'i']);
   });
 
   it('should handle array with only spaces', function() {
     var result = sentenceReverse([' ', ' ']);
 
-    expect(result).to.be.equal([' ', ' ']);
+    expect(result).to.be.eql([' ', ' ']);
   });
 
   it('should handle array with multiple spaces between words', function() {
     var result = sentenceReverse(['a', ' ', ' ', 'b', ' ', 'c']);
 
-    expect(result).to.be.equal(['c', ' ', ' ', 'b', ' ', 'c']);
+    expect(result).to.be.eql(['c', ' ', 'b', ' ', ' ', 'a']);
+  });
+
+  it('should handle array with multiple spaces in single string between words', function() {
+    var result = sentenceReverse(['a', '  ', 'b', '  ', 'c']);
+
+    expect(result).to.be.eql(['c', '  ', 'b', '  ', 'a']);
   });
 });
