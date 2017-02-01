429dd12e77b21bd518ed61b67a5c1869eb5a263c
mjstromberg <michaeljstromberg@gmail.com>
2017-02-01 15:08:58 -0800

    (solution) Fix solution to identify spaces properly

--- sentenceReverse/sentenceReverse.js
+++ sentenceReverse/sentenceReverse.js
@@ -14,24 +14,70 @@
  * 
  */
 
+var reverseWord = function reverseWord(array, startIndex, endIndex) {
+  // initialize step to be 0
+  var step = 0;
+  // initialize leftValue to be null
+  var leftValue = null;
+  // while startIndex plus step is less than endIndex minus step
+  while (startIndex + step < endIndex - step) {
+    // set leftValue to value at startIndex plus step
+    leftValue = array[startIndex + step];
+    // set value at startIndex plus step to equal value at endIndex minus step
+    array[startIndex + step] = array[endIndex - step];
+    // set value at endIndex minus step to equal leftValue
+    array[endIndex - step] = leftValue;
+    // increment step by 1
+    step++;
+  }
+};
+
 var sentenceReverse = function sentenceReverse(array) {
   // reverse entire array
+  array = array.reverse();
   // initialize inWord variable with false
+  var inWord = false;
   // initialize startIndex with null
+  var startIndex = null;
   // initialize endIndex with null
+  var endIndex = null;
   // loop through reversed array
+  for (var i = 0; i < array.length; i++) {
     // if current character is a non-space
+    if (/[^ ]+/.test(array[i])) {
       // if inWord is false
+      if (!inWord) {
         // toggle inWord
+        inWord = !inWord;
         // set startIndex to current index
+        startIndex = i;
         // set endIndex to current index
+        endIndex = i;
       // else
+      } else {
         // set endIndex to current index
+        endIndex = i;
+        // if last item in array
+        if (i === array.length - 1) {
+          // reverse word
+          reverseWord(array, startIndex, endIndex);
+        }
+      }
     // else
+    } else {
       // if inWord is true
+      if (inWord) {
         // toggle inWord
+        inWord = !inWord;
         // reverse word
+        reverseWord(array, startIndex, endIndex);
         // set startIndex to null
+        startIndex = null;
         // set endIndex to null
+        endIndex = null;
+      }
+    }
+  }
   // return array
+  return array;
 };
