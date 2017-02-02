1b8c9cf11e9e975108b91b799a4af38e220358e7
mjstromberg <michaeljstromberg@gmail.com>
2017-02-02 13:50:32 -0800

    (solution) Add initial pseudocode for quickSort

--- quickSort/quickSort.js
+++ quickSort/quickSort.js
@@ -6,6 +6,12 @@
  * 
  */
 
-var quickSort = function quickSort(array) {
-
+var quickSort = function quickSort(array, left, right) {
+  // initialize pivot as last element in array
+  // loop through array
+    // if item is less than or equal to pivot
+      // push item to left
+    // else
+      // push item to right
+  // return a concatenation of a quickSort of left and quickSort of right
 };
