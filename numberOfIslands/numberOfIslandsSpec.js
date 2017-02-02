b358a04ecc7d9092c42f2f9bfe96bb141aea1b29
mjstromberg <michaeljstromberg@gmail.com>
2017-02-01 20:01:37 -0800

    (fix) Fix numberOfIslandsSpec to have commas in matrix

--- numberOfIslands/numberOfIslandsSpec.js
+++ numberOfIslands/numberOfIslandsSpec.js
@@ -8,9 +8,9 @@ describe('numberOfIslands', function() {
 
   it('should return correct answer for example matrix', function() {
     var exampleMatrix1 = [
-      [1, 1, 1, 1, 0]
-      [1, 1, 0, 1, 0]
-      [1, 1, 0, 0, 0]
+      [1, 1, 1, 1, 0],
+      [1, 1, 0, 1, 0],
+      [1, 1, 0, 0, 0],
       [0, 0, 0, 0, 0]
     ];
     var result = numberOfIslands(exampleMatrix1);
@@ -20,9 +20,9 @@ describe('numberOfIslands', function() {
 
   it('should return correct answer for second example matrix', function() {
     var exampleMatrix2 = [
-      [1, 1, 0, 0, 0]
-      [1, 1, 0, 0, 0]
-      [0, 0, 1, 0, 0]
+      [1, 1, 0, 0, 0],
+      [1, 1, 0, 0, 0],
+      [0, 0, 1, 0, 0],
       [0, 0, 0, 1, 1]
     ];
     var result = numberOfIslands(exampleMatrix2);
@@ -32,8 +32,8 @@ describe('numberOfIslands', function() {
 
   it('should handle non-square matrices', function() {
     var exampleMatrix3 = [
-      [1, 1, 0, 0]
-      [1, 1, 0, 0]
+      [1, 1, 0, 0],
+      [1, 1, 0, 0],
       [0, 0, 1, 0]
     ];
     var result = numberOfIslands(exampleMatrix3);
@@ -43,8 +43,8 @@ describe('numberOfIslands', function() {
 
   it('should handle matrix with all water', function() {
     var exampleMatrix4 = [
-      [0, 0, 0]
-      [0, 0, 0]
+      [0, 0, 0],
+      [0, 0, 0],
       [0, 0, 0]
     ];
     var result = numberOfIslands(exampleMatrix4);
@@ -54,8 +54,8 @@ describe('numberOfIslands', function() {
 
   it('should handle an all-island matrix', function() {
     var exampleMatrix5 = [
-      [1, 1, 1]
-      [1, 1, 1]
+      [1, 1, 1],
+      [1, 1, 1],
       [1, 1, 1]
     ];
     var result = numberOfIslands(exampleMatrix5);
