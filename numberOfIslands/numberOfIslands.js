cf295270bf97a494609c6fdec78b4590829fb23c
mjstromberg <michaeljstromberg@gmail.com>
2017-02-01 18:58:49 -0800

    (feature) Add numberOfIslands toy problem

---
+++ numberOfIslands/numberOfIslands.js
@@ -0,0 +1,28 @@
+/*
+ *
+ * Given a 2d grid map of '1's (land) and '0's (water), count the number of islands.
+ * 
+ * An island is surrounded by water and is formed by connecting adjacent lands horizontally or
+ * vertically. You may assume all four edges of the grid are all surrounded by water.
+ *
+ * Example #1:
+ * Matrix:
+ * [[1, 1, 1, 1, 0]
+ *  [1, 1, 0, 1, 0]
+ *  [1, 1, 0, 0, 0]
+ *  [0, 0, 0, 0, 0]]
+ * Produces: 1
+ *
+ * Example #2:
+ * Matrix:
+ * [[1, 1, 0, 0, 0]
+ *  [1, 1, 0, 0, 0]
+ *  [0, 0, 1, 0, 0]
+ *  [0, 0, 0, 1, 1]]
+ * Produces: 3
+ * 
+ */
+
+var numberOfIslands = function numberOfIslands(matrix) {
+
+};
