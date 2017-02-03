cabdfa0e0e3299a2f38706385bb23e606e90f993
mjstromberg <michaeljstromberg@gmail.com>
2017-02-03 15:49:51 -0800

    (solution) Add solution to reverseLinkedList

--- reverseLinkedList/reverseLinkedList.js
+++ reverseLinkedList/reverseLinkedList.js
@@ -9,5 +9,24 @@
  */
 
 var reverseLinkedList = function reverseLinkedList(head) {
+  // loop through the list linearly
+  // change the pointer to point the previous node
   
+  var nodes = {
+    current: head,
+    next: head.next,
+    previous: null
+  };
+
+  while (nodes.current) {
+    // point the current node to the previous node
+    nodes.current.next = nodes.previous;
+
+    // move all node trackers one over
+    nodes.previous = nodes.current;
+    nodes.current = nodes.next;
+    nodes.next = nodes.next ? nodes.next.next : null;
+  }
+
+  return nodes.previous;
 };
