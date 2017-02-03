62d55058695fb5ae8d4b201ad92951b351a652ce
mjstromberg <michaeljstromberg@gmail.com>
2017-02-02 21:22:49 -0800

    (solution) Add pseudocode for quickSort

--- quickSort/quickSort.js
+++ quickSort/quickSort.js
@@ -6,12 +6,37 @@
  * 
  */
 
+var swap = function swap(array, left, right) {
+  // initialize tempValue with value at left
+  // set value at left equal to value at right
+  // set value at right to tempValue
+};
+
+var partition = function partition(array, left, right) {
+  // initialize middle variable
+  // initialize middleValue variable
+  // initialize leftValue variable
+  // initiallize rightValue variable
+  // while left is less than or equal to right
+    // while leftValue is less than middleValue
+      // increment left by 1
+      // set new leftValue
+    // while rightValue is greater than middleValue
+      // decrement right by 1
+      // set new rightValue
+    // if left is less than or equal to right
+      // swap leftValue and rightValue
+  // return middle
+};
+
 var quickSort = function quickSort(array, left, right) {
-  // initialize pivot as last element in array
-  // loop through array
-    // if item is less than or equal to pivot
-      // push item to left
-    // else
-      // push item to right
-  // return a concatenation of a quickSort of left and quickSort of right
+  // set left equal to 0 if no left
+  // set right equal to last index of array if no right
+  // if array length is greater than 1
+    // initialize pivot to partition invocation
+    // if left is less than pivot minus 1
+      // invoke quickSort with array, left, and index - 1
+    // if right is greater than pivot plus 1
+      // invoke quickSort with array, index + 1, right
+  // return array
 };
