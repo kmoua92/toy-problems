ab2cea5f0f34732134ea5a313f0c6dcf3cd4e8e5
mjstromberg <michaeljstromberg@gmail.com>
2017-02-06 12:12:47 -0800

    (feature) Add spec for isUnqique

---
+++ isUnique/isUniqueSpec.js
@@ -0,0 +1,32 @@
+describe('isUnique', function() {
+
+  it('should return a boolean', function() {
+    var result = isUnique(exampleArr, exampleStr);
+
+    expect(result).to.be.a('boolean');
+  });
+
+  it('should handle false example case', function() {
+    var result = isUnique('hello');
+
+    expect(result).to.be.false;
+  });
+
+  it('should handle true example case', function() {
+    var result = isUnique('abc');
+
+    expect(result).to.be.true;
+  });
+
+  it('should handle string with one character', function() {
+    var result = isUnique('a');
+
+    expect(result).to.be.true;
+  });
+
+  it('should handle empty string', function() {
+    var result = isUnique('');
+
+    expect(result).to.be.null;
+  });
+});
