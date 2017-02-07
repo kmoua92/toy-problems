ca1ea762954b7cba3a7eeb6700a5d12397bee12f
mjstromberg <michaeljstromberg@gmail.com>
2017-02-07 12:25:38 -0800

    (feature) Add palindromePermutation toy problem

---
+++ palindromePermutation/palindromePermutation.js
@@ -0,0 +1,17 @@
+/*
+ *
+ * Given a string, write a function to check if it is a permutation of a palindrome. A palindrome
+ * is a word or phrase that is the same forwards and backwards. A permutation is a rearrangement of
+ * letters. The palindrome does not need to be limited to just dictionary words.
+ *
+ * Example:
+ *   Input:
+ *     'Tact Coa'
+ *   Output:
+ *     True (permutations: 'taco cat', 'acto cta', etc.)
+ * 
+ */
+
+var palindromePermutation = function palindromePermutation(string) {
+
+};
