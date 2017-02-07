645c6e08929e7a52006617cb6750bf0ec07bc44c
mjstromberg <michaeljstromberg@gmail.com>
2017-02-06 16:25:16 -0800

    (solution) Add solution for checkPermuatation

--- checkPermutation/checkPermutation.js
+++ checkPermutation/checkPermutation.js
@@ -5,5 +5,29 @@
  */
 
 var checkPermutation = function checkPermutation(str1, str2) {
-  
+  if (str1.length !== str2.length || str1.length === 0) {
+    return false;
+  }
+
+  var str1Map = {};
+  var charToMap = '';
+  var charToCheck = '';
+
+  for (var i = 0; i < str1.length; i++) {
+    charToMap = str1.charAt(i);
+
+    str1Map[charToMap] = str1Map[charToMap] ? str1Map[charToMap] + 1 : 1;
+  }
+
+  for (var i = 0; i < str2.length; i++) {
+    charToCheck = str2.charAt(i);
+
+    if (!str1Map[charToCheck]) {
+      return false;
+    }
+
+    str1Map[charToCheck]--;
+  }
+
+  return true;
 };
