67c1fd5fd3380708ac4ca66922e8691b90c9f7d6
mjstromberg <michaeljstromberg@gmail.com>
2017-02-06 16:42:56 -0800

    (feature) Add spec for urlify

---
+++ urlify/urlifySpec.js
@@ -0,0 +1,26 @@
+describe('urlify', function() {
+
+  it('should return a string', function() {
+    var result = urlify('Mr John Smith   ', 13);
+
+    expect(result).to.be.a('string');
+  });
+
+  it('should return correct result for example', function() {
+    var result = urlify('Mr John Smith   ', 13);
+
+    expect(result).to.be.equal('Mr%20John%20Smith');
+  });
+
+  it('should handle no spaces', function() {
+    var result = urlify('abc', 3);
+
+    expect(result).to.be.eql('abc');
+  });
+
+  it('should handle all spaces', function() {
+    var result = urlify('   ', 3);
+
+    expect(result).to.be.eql('%20%20%20');
+  });
+});
