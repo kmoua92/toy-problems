d8b7a74f00c1d69dfa08b29272d9a8aed3bd3ee9
mjstromberg <michaeljstromberg@gmail.com>
2017-02-07 12:18:34 -0800

    (solution) Add solution to urlify

--- urlify/urlify.js
+++ urlify/urlify.js
@@ -6,11 +6,26 @@
  * can perform this operation in place.)
  *
  * Example:
- *   Input: 'Mr John Smith   ', 13
+ *   Input: 'Mr John Smith    ', 13
  *   Output: 'Mr%20John%20Smith'
  * 
  */
 
 var urlify = function urlify(string, trueLength) {
-  
+  var fillIndex = string.length - 1;
+  var resultArray = string.split('');
+
+  for (var lookupIndex = trueLength - 1; lookupIndex >= 0; lookupIndex--) {
+    if (resultArray[lookupIndex] === ' ') {
+      resultArray[fillIndex] = '0';
+      resultArray[fillIndex - 1] = '2';
+      resultArray[fillIndex - 2] = '%';
+
+      fillIndex -= 3;
+    } else {
+      resultArray[fillIndex--] = resultArray[lookupIndex];
+    }
+  }
+
+  return resultArray.join('');
 };
