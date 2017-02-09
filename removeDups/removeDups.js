748c27a398c4215f2e48f7664f538e4e6421cf9d
mjstromberg <michaeljstromberg@gmail.com>
2017-02-08 17:52:24 -0800

    (solution) Add solution to removeDups

--- removeDups/removeDups.js
+++ removeDups/removeDups.js
@@ -7,5 +7,24 @@
  */
 
 var removeDups = function removeDups(head) {
+  // build a map to track non-duplicate nodes
+  // if duplacte node
+    // link previous node to next node in order to bypass the current duplicate node
+  
+  var map = {};
+  var currentNode = head;
+  var previousNode = null;
 
+  while (currentNode) {
+    if (map[currentNode.value]) {
+      previousNode.next = currentNode.next;
+    } else {
+      map[currentNode.value] = true;
+      previousNode = currentNode;
+    }
+
+    currentNode = currentNode.next;
+  }
+
+  return head;
 };
