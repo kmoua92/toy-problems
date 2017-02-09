07a5ad95920fb21d4caaeff1effa7e34900a50c1
mjstromberg <michaeljstromberg@gmail.com>
2017-02-08 17:12:13 -0800

    (feature) Add removeDups toy problem

---
+++ removeDups/removeDups.js
@@ -0,0 +1,11 @@
+/*
+ *
+ * Write code to remove duplicates from an unsorted linked list.
+ *
+ * Follow up: How would you solve this problem if a temporary buffer is not allowed?
+ * 
+ */
+
+var removeDups = function removeDups(head) {
+
+};
