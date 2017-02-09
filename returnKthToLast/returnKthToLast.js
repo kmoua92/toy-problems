2acd24d92f216eb4fb6ea4ba82097f26e14eb982
mjstromberg <michaeljstromberg@gmail.com>
2017-02-09 13:20:49 -0800

    (solution) Add solution to returnKthToLast

--- returnKthToLast/returnKthToLast.js
+++ returnKthToLast/returnKthToLast.js
@@ -5,5 +5,22 @@
  */
 
 var returnKthToLast = function returnKthToLast(head, k) {
+  // create two pointers separated by k nodes
+  // iterate through until the front pointer hits the last node
 
+  var front = head;
+  var back = head;
+  var separation = 0;
+
+  while (front.next) {
+    if (separation === k - 1) {
+      back = back.next;
+    } else {
+      separation++;
+    }
+
+    front = front.next;
+  }
+
+  return back;
 };
